303a4d7cd6b9f3a2e113d9a2aeebbe8898c15ddd
AShpachkov <151773979+AShpachkov@users.noreply.github.com>
2025-01-28 20:17:08 +0500

    Add files via upload

---
+++ ZOV.js
@@ -0,0 +1,22 @@
+const RRchat = {
+    colors: ["ffffff", "0000ff", "ffff0000"],
+    colorIndex: 0,
+
+    init() {
+        let _onChatMessage = window.onChatMessage;
+
+        window.onChatMessage = (text, color) => {
+            let chat = window.interface("Hud")?.$refs.chat;
+            if (!chat) return _onChatMessage.call(this, text, color);
+
+            if (text.includes("[Компания]")) {
+                color = this.colors[this.colorIndex];
+                this.colorIndex = (this.colorIndex + 1) % this.colors.length;
+            }
+
+            _onChatMessage.call(this, text, color);
+        };
+
+        interface("Hud")?.$refs?.chat?.add("Счетчик /z", "65c466");
+    }
+};
